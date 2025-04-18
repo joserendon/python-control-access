af5246e491ff84f53b4d5b6ec0abc270612e97b2
Jose Rendon <jose.rendon@praxedes.co>
2025-04-18 12:09:35 -0500

    refactor Estados class to rename 'estado' field to 'name'

--- Entidades/Estados.py
+++ Entidades/Estados.py
@@ -1,9 +1,9 @@
 class Estados:
     def __init__(self):
         self.id = 0
-        self.estado = None
+        self.name = None
 
     def get_id(self): return self.id
     def set_id(self, value): self.id = value
-    def get_estado(self): return self.estado
-    def set_estado(self, value): self.estado = value
+    def get_estado(self): return self.name
+    def set_estado(self, value): self.name = value
