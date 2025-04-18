7277cefc1f58edd523e767a8021042a68cc86653
Jose Rendon <jose.rendon@praxedes.co>
2025-04-18 12:30:08 -0500

    refactor Estados and EstadosRepositorio to unify naming conventions for 'nombre' field and methods

--- Entidades/Estados.py
+++ Entidades/Estados.py
@@ -1,9 +1,9 @@
 class Estados:
     def __init__(self):
         self.id = 0
-        self.name = None
+        self.nombre = None
 
     def get_id(self): return self.id
     def set_id(self, value): self.id = value
-    def get_name(self): return self.name
-    def set_name(self, value): self.name = value
+    def get_nombre(self): return self.nombre
+    def set_nombre(self, value): self.nombre = value
--- Repositorios/EstadosRepositorio.py
+++ Repositorios/EstadosRepositorio.py
@@ -3,7 +3,7 @@ from Repositorios.BaseRepositorio import BaseRepositorio
 
 class EstadosRepositorio(BaseRepositorio):
 
-    def obtener_estados(self) -> list[Estados.Estados]:
+    def obtener(self) -> list[Estados.Estados]:
         query = "{CALL proc_select_estados()}"
         rows = self.obtener_todos(query)
 
@@ -11,19 +11,19 @@ class EstadosRepositorio(BaseRepositorio):
         for elemento in rows:
             entidad: Estados = Estados.Estados();
             entidad.set_id(elemento[0]);
-            entidad.set_name(elemento[1]);
+            entidad.set_nombre(elemento[1]);
             estados.append(entidad);
 
         return estados   
    
-    def insertar_estado(self, estado: str) -> str:
+    def insertar(self, estado: str) -> str:
         query = "{CALL proc_insert_estados(?, @Respuesta)}"
         return self.ejecutar_sp(query, (estado,))
 
-    def actualizar_estado(self, id_estado: int, nuevo_estado: str) -> str:
+    def actualizar(self, id_estado: int, nuevo_estado: str) -> str:
         query = "{CALL proc_update_estados(?,?, @Respuesta)}"
         return self.ejecutar_sp(query, (id_estado, nuevo_estado))
 
-    def eliminar_estado(self, id_estado: int) -> str:
+    def eliminar(self, id_estado: int) -> str:
         query = "{CALL proc_delete_estados(?, @Respuesta)}"
         return self.ejecutar_sp(query, (id_estado,)) 
